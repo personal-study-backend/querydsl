b79a818f993041f87baebed7e6419969fe329975
sangwoo0727 <kangsw9395@gmail.com>
2021-09-24 13:34:10 +0900

    add: case test 추가

--- src/test/java/hello/noddy/querydsl/QuerydslBasicTest.java
+++ src/test/java/hello/noddy/querydsl/QuerydslBasicTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -374,6 +375,36 @@ public class QuerydslBasicTest {
 
     for (Tuple tuple : fetch) {
       System.out.println(tuple);
+    }
+  }
+
+  @Test
+  void basicCaseTest() {
+    List<String> result = queryFactory
+        .select(member.age
+            .when(10).then("열살")
+            .when(20).then("스무살")
+            .otherwise("기타"))
+        .from(member)
+        .fetch();
+
+    for (String s : result) {
+      System.out.println(s);
+    }
+  }
+
+  @Test
+  void complexCaseTest() {
+    List<String> result = queryFactory
+        .select(new CaseBuilder()
+            .when(member.age.between(0, 20)).then("0~20살")
+            .when(member.age.between(21, 30)).then("21~30살")
+            .otherwise("기타"))
+        .from(member)
+        .fetch();
+
+    for (String s : result) {
+      System.out.println(s);
     }
   }
 }
